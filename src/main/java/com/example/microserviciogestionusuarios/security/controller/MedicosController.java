b3a7f5e9f0125654afd44666bebd8d44cdb5dbd7
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-05-29 04:33:17 -0400

    [Add] obtener listado de medicos

--- src/main/java/com/example/microserviciogestionusuarios/security/controller/MedicosController.java
+++ src/main/java/com/example/microserviciogestionusuarios/security/controller/MedicosController.java
@@ -1,10 +1,29 @@
 package com.example.microserviciogestionusuarios.security.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.microserviciogestionusuarios.security.entities.MedicoEntity;
+import com.example.microserviciogestionusuarios.security.repositories.MedicoRepository;
+
+import jakarta.annotation.security.PermitAll;
 
 @RestController
 @RequestMapping(path="/medicos")
 public class MedicosController {
-    
+    @Autowired
+    MedicoRepository medicoRepository;
+
+    @PermitAll
+    @GetMapping
+    public ResponseEntity<List<MedicoEntity>> listadoMedicos() {
+        List<MedicoEntity> listadoMedico=medicoRepository.findAll();
+        return new ResponseEntity<List<MedicoEntity>>(listadoMedico, HttpStatus.OK);
+    }
 }
